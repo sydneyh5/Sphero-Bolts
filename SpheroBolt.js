fa8b86b6da3d4fe5f9c83d7929fae6afd653b229
Sydney <hribar.sydney@gmail.com>
2023-02-13 08:21:04 -0500

    Blue

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -1,3 +1,14 @@
 //Programming Team: Sydney Hribar and Gabe Westra
 //Date: 2.6.2023
 //Project: Sphero Bolt Maze
+
+async function blue() {
+    await roll ((getHeading() + 0), 73, 2)
+    await spin(90, 2)
+    setMainLed({ r: 0, g: 0, b: 200 })
+}
+
+
+async function startProgram() {
+    await blue()
+} 
