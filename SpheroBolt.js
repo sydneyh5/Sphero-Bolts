3dee41275a47aac99fdda67ba0e9b92567a09c3f
Sydney <hribar.sydney@gmail.com>
2023-02-14 08:40:11 -0500

    Sound Two Commit

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -17,15 +17,23 @@ async function soundOne (){
 
 async function red(){
     await spin(40, 2)
-    await roll ((getHeading() + 0), 43, 2)
+    await roll ((getHeading() + 0), 40, 2)
     await spin(270, 2)
     setMainLed({ r: 225, g: 0, b: 0 })
-    await roll ((getHeading() + 0), 30, 2)
+    await roll ((getHeading() + 0), 25, 2)
 }
 
+async function soundTwo(){
+    await spin(270, 2)
+    await roll ((getHeading() + 0), 39, 2)
+    await spin(90, 2)
+    await roll ((getHeading() + 0), 27, 2)
+    await Sound.Animal.HorseGallop.play()
+}
 
 async function startProgram() {
     await blue()
     await soundOne()
     await red()
+    await soundTwo()
 } 
