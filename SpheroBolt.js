69ae974b80f88db31b2b630bd5595bdd8b71b19f
Sydney <hribar.sydney@gmail.com>
2023-02-21 07:49:02 -0500

    Checkpoint Description Comments

--- SpheroBolt.js
+++ SpheroBolt.js
@@ -2,12 +2,14 @@
 //Date: 2.6.2023
 //Project: Sphero Bolt Maze
 
+// Blue Checkpoint (The first  checkpoint of the maze/the start)
 async function blue() {
     await roll ((getHeading() + 0), 73, 2)
     await spin(90, 2)
     setMainLed({ r: 0, g: 0, b: 200 })
 }
 
+//First Sound Checkpoint
 async function soundOne (){
     await roll ((getHeading() + 0), 49, 2)
     await spin(90, 2)
@@ -15,6 +17,7 @@ async function soundOne (){
     await Sound.Animal.Alligator.play()
 }
 
+//Red Checkpoint
 async function red(){
     await spin(45, 2)
     await roll ((getHeading() + 0), 41, 2)
@@ -23,6 +26,7 @@ async function red(){
     await roll ((getHeading() + 0), 25, 2)
 }
 
+//Second Sound Checkpoint
 async function soundTwo(){
     await spin(270, 2)
     await roll ((getHeading() + 0), 40, 2)
@@ -31,6 +35,7 @@ async function soundTwo(){
     await Sound.Animal.Horse.play()
 }
 
+//Green Checkpoint 
 async function green(){
     await spin(45, 2)
     await roll ((getHeading() + 0), 23, 2)
@@ -39,6 +44,7 @@ async function green(){
     await roll ((getHeading() + 0), 33, 2)
 }
 
+//Third Sound Checkpoint 
 async function soundThree(){
     await spin (270, 2)
     await roll ((getHeading() + 0), 30, 2)
@@ -47,6 +53,7 @@ async function soundThree(){
     await Sound.Animal.Monkey.play()
 }
 
+// Purple Checkpoint (The end of the maze)
 async function purple(){
     await spin (37, 2)
     await roll ((getHeading() + 0), 40, 2)
